fdd2a4a7003ab5c3970387c9f7c29e09aa3e241b
jamesf816-web <jamesf816@gmail.com>
2025-12-03 15:40:07 -0500

    Update app host to '127.0.0.1'
    Change the host from '0.0.0.0' to '127.0.0.1' for local testing.

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ from callbacks import simulation_callbacks
 from callbacks import results_callbacks
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='127.0.0.1', port=8050)
 
