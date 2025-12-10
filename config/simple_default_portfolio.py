0878ba1927eadfd8075b7bd773fd6197330d550e
waiwebsitecommittee-lab <waiwebsitecommittee@gmail.com>
2025-12-09 19:46:57 -0500

    First Fully Operational Modularized Code

---
+++ config/simple_default_portfolio.py
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<portfolio>
+    <account name="GST_Exempt">
+        <balance>3000000</balance>
+        <equity>0.75</equity>
+        <bond>0.25</bond>
+        <tax>trust</tax>
+        <owner>person1</owner>
+        <income>0.015</income>
+    </account>
+
+    <account name="Deferred457b">
+        <balance>30000</balance>
+        <equity>0.25</equity>
+        <bond>0.75</bond>
+        <tax>def457b</tax>
+        <owner>person1</owner>
+    </account>
+
+    <account name="Taxable">
+        <balance>1500000</balance>
+        <equity>0.92</equity>
+        <bond>0.08</bond>
+        <tax>taxable</tax>
+        <owner>person1</owner>
+        <basis>789874</basis>
+    </account>
+
+    <account name="Trad_IRA">
+        <balance>300000</balance>
+        <equity>0.82</equity>
+        <bond>0.18</bond>
+        <tax>traditional</tax>
+        <owner>person1</owner>
+    </account>
+
+    <account name="Inherited_IRA63">
+        <balance>1000000</balance>
+        <equity>0.81</equity>
+        <bond>0.19</bond>
+        <tax>inherited</tax>
+        <owner>person1</owner>
+	<death_year>2012</death_year>
+	<death_month>12</death_month>
+	<decedent_started_rmds>True</decedent_started_rmds>
+    </account>
+
+    <account name="Roth_IRA">
+        <balance>200000</balance>
+        <equity>1.00</equity>
+        <bond>0.00</bond>
+        <tax>roth</tax>
+        <owner>person1</owner>
+    </account>
+    
+</portfolio>
